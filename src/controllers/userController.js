7616a957db7e5972791d9f721879b71cf388c725
hyuk3800 <76979730+hyuk3800@users.noreply.github.com>
2021-07-30 19:24:16 +0900

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,3 @@
-export const indexhome = (req, res) => res.render("home", { pageTitle :  });
+export const indexhome = (req, res) => res.render("home");
 
 export const login = (req, res) => res.send("login page");
